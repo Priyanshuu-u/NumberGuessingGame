f2d68935c4684110aadcecad819604a241e1af06
Priyanshu Rajpurohit <officialpriyanshu14@gmail.com>
2024-07-29 11:38:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+
+let startButton = document.querySelector("#startbutton");
+startButton.addEventListener("click", function(){
+    let range = document.querySelector("#range");
+    let label = document.querySelector("label");
+
+    console.log(range.value);
+    if(!(range.value >=0)){
+        label.innerText="Please enter a value greater than zero";
+    }
+    else{
+
+   
+    let maxNumber = range.value;
+    let random = Math.floor(Math.random() * maxNumber) + 1;
+    console.log(random);
+    startButton.classList.add("startbuttongo");
+    range.classList.add("afterrange");
+    label.classList.add("afterrange");
+
+    let hint = document.querySelector(".hint");
+    let guess = document.querySelector(".guess");
+    let submit = document.querySelector(".submit")
+    hint.classList.remove("beforestart");
+    guess.classList.remove("beforestart");
+    submit.classList.remove("beforestart");
+    hint.innerText=`Enter your guess between 1 and ${range.value} inclusive`;
+    let attempt = document.querySelector(".attempts");
+            let count=1;
+   
+        submit.addEventListener("click",function(event){
+            let guessed = guess.value;
+            console.log(guessed);
+            
+            attempt.innerText = `Attempts: ${count++}`;
+            
+            
+                
+            if(guessed == random){
+                hint.innerText=`You won!!! Value is ${guessed}`;
+                let img = document.querySelector("img");
+                img.setAttribute("src","https://png.pngtree.com/png-vector/20220521/ourmid/pngtree-comic-speech-bubbles-with-text-you-win-png-image_4667123.png");
+                let arr = ["#d9a2ff", "#fbb5ff", "#ffe4f0", "#a2ceff", "#ff9595"];
+                let body = document.querySelector("body");
+                let index = 0;
+        
+                setInterval(function() {
+                    body.style.backgroundColor = arr[index];
+                    index = (index + 1) % arr.length;
+                }, 500);
+                
+            }
+            else if(guessed > random){
+                hint.innerText=`Value is lower than ${guessed}`;
+            }
+            
+            else{
+                hint.innerText=`Value is greater than ${guessed}`;
+            }
+            
+     
+
+        
+        })
+    }
+})
